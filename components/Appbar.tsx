c093201155b2227b050f7603cb020a62e2bed92c
Juan Briceno <juanbri@gmx.com>
2023-01-21 09:55:22 +0000

    banner

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -10,92 +10,101 @@ import {
 
 const Appbar = () => {
   return (
-    <div className="bg-[#fffffb] items-center justify-between flex p-4 sticky top-0 z-10">
-      <div className="">
-        <Link
-          className="flex hover:scale-110 transform-gpu ease-in-out duration-300 rounded-full"
-          href={"/"}
-        >
-          <img src="/logo.svg" className="w-[32px] h-[32px] shadow-xl" />
-          <h1 className="header self-center ml-1.5 logo">Trovali</h1>
-        </Link>
-      </div>
+    <div className="bg-[#fffffb] py-1 lg:py-3 sticky top-0 z-10 shadow-x">
+      <div className="lg:flex lg:justify-center lg:items-center">
+        <div className="px-4 lg:px-8 w-auto lg:w-[1130px]">
+          <div className="flex justify-between items-center">
+            <div>
+              <Link
+                className="flex hover:scale-110 transform-gpu ease-in-out duration-300 rounded-full"
+                href={"/"}
+              >
+                <img src="/logo.svg" className="w-[32px] h-[32px] shadow-xl" />
+                <h3 className="header self-center ml-1.5 logo">Trovali</h3>
+              </Link>
+            </div>
 
-      {/* Grande */}
-      <div className="hidden md:flex">
-        <Link className="" href={"/about"}>
-          About
-        </Link>
-        <Link className="px-4" href={"/features"}>
-          Features
-        </Link>
-        <Link className="" href={"/benefits"}>
-          Benefits
-        </Link>
-        <Link className="px-4" href={"/pricing"}>
-          Pricing
-        </Link>
-        <Link className="pr-4" href={"/news"}>
-          News
-        </Link>
-        <div className="hover:scale-110 transform-gpu ease-in-out duration-300">
-          <Link
-            className="bg-blue text-white py-2 rounded-[6px] px-4 "
-            href={"/contact"}
-          >
-            Contact
-          </Link>
-        </div>
-      </div>
-
-      {/* Pequeno */}
-      <div className="block md:hidden">
-        <Menu>
-          <div className="hover:scale-110 transform-gpu ease-in-out duration-300">
-            <MenuButton
-              as={IconButton}
-              aria-label="Options"
-              icon={
-                <Image src="/hamburguer.png" width={23} height={23} alt={""} />
-              }
-              variant="filled"
-            />
-          </div>
-          <MenuList minW={"30px"} className="flex-col flex">
-            <MenuItem>
-              <Link className="pl-4 py-1" href={"/about"}>
+            {/* Grande */}
+            <div className="hidden md:flex items-center">
+              <Link className="" href={"/about"}>
                 About
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link className="pl-4 py-1" href={"/features"}>
+              <Link className="px-4" href={"/features"}>
                 Features
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link className="pl-4 py-1" href={"/benefits"}>
+              <Link className="" href={"/benefits"}>
                 Benefits
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link className="pl-4 py-1" href={"/pricing"}>
+              <Link className="px-4" href={"/pricing"}>
                 Pricing
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link className="pl-4 py-1" href={"/news"}>
+              <Link className="pr-4" href={"/news"}>
                 News
               </Link>
-            </MenuItem>
+              <Link
+                className="bg-blue hover:bg-[#1b1393] text-white py-2 rounded-[6px] px-4 "
+                href={"/contact"}
+              >
+                Contact
+              </Link>
+            </div>
 
-            <Link
-              className="bg-blue text-white py-2 rounded-[6px] px-4 mx-4 my-1 w-[88px]"
-              href={"/contact"}
-            >
-              Contact
-            </Link>
-          </MenuList>
-        </Menu>
+            {/* Pequeno */}
+            <div className="block md:hidden">
+              <Menu>
+                <div className="hover:scale-110 transform-gpu ease-in-out duration-300">
+                  <MenuButton
+                    as={IconButton}
+                    aria-label="Options"
+                    icon={
+                      <Image
+                        src="/hamburguer.png"
+                        width={23}
+                        height={23}
+                        alt={""}
+                      />
+                    }
+                    variant="filled"
+                  />
+                </div>
+                <MenuList minW={"30px"} className="flex-col flex">
+                  <MenuItem>
+                    <Link className="pl-4 py-1" href={"/about"}>
+                      About
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link className="pl-4 py-1" href={"/features"}>
+                      Features
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link className="pl-4 py-1" href={"/benefits"}>
+                      Benefits
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link className="pl-4 py-1" href={"/pricing"}>
+                      Pricing
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link className="pl-4 py-1" href={"/news"}>
+                      News
+                    </Link>
+                  </MenuItem>
+
+                  <Link
+                    className="bg-blue text-white py-2 rounded-[6px] px-4 mx-4 my-1 w-[88px]"
+                    href={"/contact"}
+                  >
+                    Contact
+                  </Link>
+                </MenuList>
+              </Menu>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,9 @@ export default function Layout({ children }: { children: any }) {
   return (
     <>
       <Appbar />
-      {children}
+      <div className="lg:flex lg:justify-center lg:items-center">
+        <div className="px-4 lg:px-8 w-auto lg:w-[1130px] ">{children}</div>
+      </div>
     </>
   );
 }
